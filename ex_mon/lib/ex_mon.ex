ed2d10bb8c370ba57550f3b15120bee3d2ef870e
Felipe Cepluki <felipecepluki@gmail.com>
2023-04-17 20:03:07 -0300

    Printing game state and checking player movements

--- ex_mon/lib/ex_mon.ex
+++ ex_mon/lib/ex_mon.ex
@@ -1,12 +1,17 @@
 defmodule ExMon do
   alias ExMon.{Game, Player}
+  alias ExMon.Game.{Actions, Status}
   @computer_name "Robotinik"
   def create_player(name, move_avg, move_rnd, move_heal) do
     Player.build(name, move_rnd, move_avg, move_heal)
   end
   def start_game(player) do
-   @computer_name
+    @computer_name
     |> create_player(:punch, :kick, :heal)
     |> Game.start(player)
+    Status.print_round_message()
+  end
+  def make_move(move) do
+    Actions.fetch_move(move)
   end
 end
--- ex_mon/lib/player.ex
+++ ex_mon/lib/player.ex
@@ -1,14 +1,16 @@
 defmodule ExMon.Player do
-  @required_keys [:life, :move_avg, :move_heal, :move_rnd, :name]
+  @required_keys [:life, :moves, :name]
   @max_life 100
   @enforce_keys @required_keys
   defstruct @required_keys
   def build(name, move_rnd, move_avg, move_heal) do
     %ExMon.Player{
       life: @max_life,
-      move_rnd: move_rnd,
-      move_avg: move_avg,
-      move_heal: move_heal,
+      moves: %{
+        move_avg: move_avg,
+        move_heal: move_heal,
+        move_rnd: move_rnd,
+      },
       name: name
     }
   end
