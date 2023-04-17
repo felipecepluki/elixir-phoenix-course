4e2980d0d7e2ebaeced4a27ed47e8be014abae35
Felipe Cepluki <felipecepluki@gmail.com>
2023-04-17 18:53:32 -0300

    Creating our Game Agent

--- ex_mon/lib/ex_mon.ex
+++ ex_mon/lib/ex_mon.ex
@@ -1,6 +1,12 @@
 defmodule ExMon do
-  alias ExMon.Player
+  alias ExMon.{Game, Player}
+  @computer_name "Robotinik"
   def create_player(name, move_avg, move_rnd, move_heal) do
-    ExMon.Player.build(name, move_rnd, move_avg, move_heal)
+    Player.build(name, move_rnd, move_avg, move_heal)
+  end
+  def start_game(player) do
+   @computer_name
+    |> create_player(:punch, :kick, :heal)
+    |> Game.start(player)
   end
 end
