f7cf90d46e23d6572cfc1ec4711fbd325c192faf
Felipe Cepluki <felipecepluki@gmail.com>
2023-04-20 22:38:58 -0300

    Printing the attack message

--- ex_mon/lib/game/actions/attack.ex
+++ ex_mon/lib/game/actions/attack.ex
@@ -1,5 +1,6 @@
 defmodule ExMon.Game.Actions.Attack do
   alias ExMon.Game
+  alias ExMon.Game.Status
 
   @move_avg_power 18..25
   @move_rnd_power 10..35
@@ -10,23 +11,25 @@ defmodule ExMon.Game.Actions.Attack do
     |> Game.fetch_player()
     |> Map.get(:life)
     |> calculate_total_life(damage)
-    |> update_opponnent_life(opponnent)
+    |> update_opponnent_life(opponnent, damage)
   end
 
   defp calculate_power(:move_avg), do: Enum.random(@move_avg_power)
   defp calculate_power(:move_rnd), do: Enum.random(@move_rnd_power)
   defp calculate_total_life(life, damage) when life - damage < 0, do: 0
   defp calculate_total_life(life, damage), do: life - damage
-  defp update_opponnent_life(life, opponnent) do
+  defp update_opponnent_life(life, opponnent, damage) do
     opponnent
     |> Game.fetch_player()
     |> Map.put(:life, life)
-    |> update_game(opponnent)
+    |> update_game(opponnent, damage)
   end
 
-  defp update_game(player, opponnent) do
+  defp update_game(player, opponnent, damage) do
     Game.info()
     |> Map.put(opponnent, player)
     |> Game.update()
+
+    Status.print_move_message(opponnent, :attack, damage)
   end
 end
