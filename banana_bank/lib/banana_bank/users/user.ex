1e9a77bc260a73c87a2d06b480212a9380fdf964
Felipe Cepluki <felipecepluki@gmail.com>
2023-07-10 11:16:19 -0300

    feat(banana_bank): evolving Users changeset

--- banana_bank/lib/banana_bank/users/user.ex
+++ banana_bank/lib/banana_bank/users/user.ex
@@ -2,6 +2,8 @@ defmodule BananaBank.Users.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @required_params [:name, :password_hash, :email, :cep]
+
   schema "users" do
     field :name, :string
     field :password_hash, :string
@@ -11,10 +13,12 @@ defmodule BananaBank.Users.User do
     timestamps()
   end
 
-  %{name: "Felipe"}
   def changeset(user \\ %__MODULE__{}, params) do
     user
-    |> cast(params, [:name, :password_hash, :email, :cep])
-    |> validate_required([:name, :password_hash, :email, :cep])
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> validate_length(:name, min: 3)
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:cep, is: 8)
   end
 end
