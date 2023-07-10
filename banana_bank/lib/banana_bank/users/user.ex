499d3dd8c154d6f094add99c5b8c03600b8c3334
Felipe Cepluki <felipecepluki@gmail.com>
2023-07-10 10:59:44 -0300

    feat(banana_bank): create the User schema

---
+++ banana_bank/lib/banana_bank/users/user.ex
@@ -0,0 +1,20 @@
+defmodule BananaBank.Users.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :name, :string
+    field :password_hash, :string
+    field :email, :string
+    field :cep, :string
+
+    timestamps()
+  end
+
+  %{name: "Felipe"}
+  def changeset(user \\ %__MODULE__{}, params) do
+    user
+    |> cast(params, [:name, :password_hash, :email, :cep])
+    |> validate_required([:name, :password_hash, :email, :cep])
+  end
+end
