fa62dc57039f263219f21faac6b0477b1a2f24e3
Felipe Cepluki <felipecepluki@gmail.com>
2023-07-13 17:13:34 -0300

    feat(banana_bank): create our client

---
+++ banana_bank/lib/banana_bank/via_cep/client.ex
@@ -0,0 +1,28 @@
+defmodule BananaBank.ViaCep.Client do
+  use Tesla
+
+  plug Tesla.Middleware.BaseUrl, "https://viacep.com.br/ws"
+  plug Tesla.Middleware.JSON
+
+  def call(cep) do
+    "/#{cep}/json"
+    |> get()
+    |> handle_response()
+  end
+
+  defp handle_response({:ok, %Tesla.Env{status: 200, body: %{"erro": true}}}) do
+    {:error, :not_found}
+  end
+
+  defp handle_response({:ok, %Tesla.Env{status: 200, body: body}}) do
+    {:ok, body}
+  end
+
+  defp handle_response({:ok, %Tesla.Env{status: 400}}) do
+    {:error, :bad_request}
+  end
+
+  defp handle_response({:error, _}) do
+    {:error, :internal_server_error}
+  end
+end
