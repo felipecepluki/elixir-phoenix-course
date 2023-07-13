e387565999659e197565d8615163bb13fa64624b
Felipe Cepluki <felipecepluki@gmail.com>
2023-07-13 16:31:37 -0300

    feat(banana_bank): testing controller part 2

--- banana_bank/test/banana_bank_web/controllers/users_controller_test.exs
+++ banana_bank/test/banana_bank_web/controllers/users_controller_test.exs
@@ -19,5 +19,21 @@ defmodule BananaBankWeb.UsersControllerTest do
         "message" => "User created with success!"
       } = response
     end
+    test "when there are invalid params, returns an error", %{conn: conn} do
+      params = %{
+        name: nil,
+        cep: "12",
+        email: "joao@banana.com",
+        password: "123456"
+      }
+      response =
+        conn
+        |> post(~p"/api/users", params)
+        |> json_response(:bad_request)
+
+      expected_response = %{"errors" => %{"cep" => ["should be 8 character(s)"], "name" => ["can't be blank"]}}
+
+      assert response = expected_response
+    end
   end
 end
