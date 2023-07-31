3cfa9a51a9ea69109fe6d7348298397481e25a89
Felipe Cepluki <felipecepluki@gmail.com>
2023-07-31 15:27:28 -0300

    feat(bananex): Genserver handle cast

--- bananex/lib/bananex/my_qeue.ex
+++ bananex/lib/bananex/my_qeue.ex
@@ -12,10 +12,20 @@ defmodule Bananex.MyQeue do
     {:reply, nil, []}
   end
 
+  def handle_call(:queue, _from, state) do
+    {:reply, state, state}
+  end
+
+  def handle_cast({:enqueue, value}, state) do
+    {:noreply, state ++ [value]}
+  end
+
   # Client
   def start_link(state \\ []) do
     GenServer.start_link(__MODULE__, state, name: __MODULE__)
   end
 
+  def queue, do: GenServer.call(__MODULE__, :queue)
   def dequeue, do: GenServer.call(__MODULE__, :dequeue)
+  def enqueue(value), do: GenServer.cast(__MODULE__, {:enqueue, value})
 end
